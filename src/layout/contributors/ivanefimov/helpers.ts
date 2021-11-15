9b6e35feb0d16e7d56413a8e3a1d4b780c7b388c
py-cs <e.1@mail.ru>
2021-11-15 10:05:17 +0300

    feat: add tests

--- src/layout/contributors/ivanefimov/helpers.ts
+++ src/layout/contributors/ivanefimov/helpers.ts
@@ -1,3 +1,3 @@
-import { BASE_URL, CardSize } from 'store/contributors/ivanefimov/constants';
+import { BASE_URL, CardSize } from '../../../store/contributors/ivanefimov/constants';
 
 export const getUrlById = (id: string) => `url(${BASE_URL}/id/${id}/${CardSize.WIDTH}/${CardSize.HEIGHT})`;
