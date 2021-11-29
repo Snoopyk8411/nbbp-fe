03073b8f532773612fc14845b05c92064a98e8a0
Aleksei Noskov <aleksei_noskov@epam.com>
2021-11-30 02:29:23 +0300

    extended mocked data

--- src/mock-data/aleksei/menu-items.ts
+++ src/mock-data/aleksei/menu-items.ts
@@ -1,4 +1,5 @@
 import { IMenuItemModel } from 'components/aleksei/menu/menu';
+import { Appearance } from 'tools/types/api-catalog-items-types';
 
 const getNextId = ((): (() => number) => {
   let nextId = 1;
@@ -9,54 +10,78 @@ const menuItems: IMenuItemModel[] = [
   {
     id: getNextId(),
     name: 'Новинки',
+    url: '',
+    appearance: Appearance.PLAIN,
     children: [
       {
         id: getNextId(),
         name: 'Алкоголь',
+        url: '',
+        appearance: Appearance.PLAIN,
         children: [
           {
             id: getNextId(),
             name: 'Пиво, медовуха',
+            url: '',
+            appearance: Appearance.PLAIN,
           },
         ],
       },
       {
         id: getNextId(),
         name: 'Бакалея',
+        url: '',
+        appearance: Appearance.PLAIN,
         children: [
           {
             id: getNextId(),
             name: 'Макароны, крупы, бобовые',
+            url: '',
+            appearance: Appearance.PLAIN,
           },
           {
             id: getNextId(),
             name: 'Мюсли',
+            url: '',
+            appearance: Appearance.PLAIN,
           },
           {
             id: getNextId(),
             name: 'Орехи, сухофрукты, ореховые пасты',
+            url: '',
+            appearance: Appearance.PLAIN,
           },
           {
             id: getNextId(),
             name: 'Растительные масла, соусы, уксус',
+            url: '',
+            appearance: Appearance.PLAIN,
           },
           {
             id: getNextId(),
             name: 'Снеки, батончики',
+            url: '',
+            appearance: Appearance.PLAIN,
           },
           {
             id: getNextId(),
             name: 'Чай, травы, кофе, какао',
+            url: '',
+            appearance: Appearance.PLAIN,
           },
         ],
       },
       {
         id: getNextId(),
         name: 'Вместо мяса',
+        url: '',
+        appearance: Appearance.PLAIN,
         children: [
           {
             id: getNextId(),
             name: 'Колбасы, сосиски, котлеты',
+            url: '',
+            appearance: Appearance.PLAIN,
           },
         ],
       },
@@ -65,20 +90,28 @@ const menuItems: IMenuItemModel[] = [
   {
     id: getNextId(),
     name: 'Хиты',
+    url: '',
+    appearance: Appearance.PLAIN,
     children: [
       {
         id: getNextId(),
         name: 'Овощи и фрукты',
+        url: '',
+        appearance: Appearance.PLAIN,
       },
       {
         id: getNextId(),
         name: 'Готовые блюда',
+        url: '',
+        appearance: Appearance.PLAIN,
       },
     ],
   },
   {
     id: getNextId(),
     name: 'Рационы на каждый день',
+    url: '',
+    appearance: Appearance.ACCENT,
   },
 ];
 
