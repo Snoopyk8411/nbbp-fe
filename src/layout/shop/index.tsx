512bd9f0abd836ca48baf5f2fb8bfb17eb7c81d6
Ilia Kotov <ilia_kotov@epam.com>
2021-12-08 03:17:55 +0300

    fix: undo changes in main

--- src/layout/shop/index.tsx
+++ src/layout/shop/index.tsx
@@ -1,5 +1,3 @@
-import { Breadcrumbs } from 'components/Breadcrumbs/Breadcrumbs';
-
 interface ShopPageLayoutProps {
   title: string;
 }
@@ -8,13 +6,6 @@ const ShopPageLayout = ({ title }: ShopPageLayoutProps): JSX.Element => {
   return (
     <div>
       <h1>{title}</h1>
-      <Breadcrumbs
-        path={[
-          { name: 'test', url: 'test' },
-          { name: 'test2', url: 'test' },
-          { name: 'test3', url: 'aa' },
-        ]}
-      />
     </div>
   );
 };
