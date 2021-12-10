8a54a1b42ccc5bae65720e6a027151bd45987957
py-cs <e.1@mail.ru>
2021-12-10 10:34:23 +0300

    feat: add geo component

--- src/layout/shop/index.tsx
+++ src/layout/shop/index.tsx
@@ -1,3 +1,5 @@
+import { Geo } from 'components/geo/geo';
+
 interface ShopPageLayoutProps {
   title: string;
 }
@@ -6,6 +8,7 @@ const ShopPageLayout = ({ title }: ShopPageLayoutProps): JSX.Element => {
   return (
     <div>
       <h1>{title}</h1>
+      <Geo />
     </div>
   );
 };
