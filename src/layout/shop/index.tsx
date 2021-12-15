c996efb3da262262fc2c378042eec363f04da150
py-cs <e.1@mail.ru>
2021-12-15 11:18:20 +0300

    fix: remove test component

--- src/layout/shop/index.tsx
+++ src/layout/shop/index.tsx
@@ -1,5 +1,3 @@
-import { GeoWidget } from 'components/geo-widget/geo-widget';
-
 interface ShopPageLayoutProps {
   title: string;
 }
@@ -8,7 +6,6 @@ const ShopPageLayout = ({ title }: ShopPageLayoutProps): JSX.Element => {
   return (
     <div>
       <h1>{title}</h1>
-      <GeoWidget />
     </div>
   );
 };
